d809ad5bad0cd3224a4f468bbc0c45695c1709db
Chen-Lin-106 <linchen1227106@outlook.com>
2022-11-10 15:09:48 +1000

    add button to change read status

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ const isRead = document.querySelector("#isRead");
 const titleForm = document.querySelector("#form-title");
 const authorForm = document.querySelector("#form-author");
 const pagesForm = document.querySelector("#form-pages");
+const read = document.querySelector(".form-read");
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
 function displayBook() {
@@ -35,6 +37,13 @@ function displayBook() {
   const title = document.createElement("p");
   const author = document.createElement("p");
   const pages = document.createElement("p");
+  const readShow = document.createElement("p");
+  // const readImage = document.createElement("img");
+  // readImage.src = "/images/check.png";
+  const readBtn = document.createElement("button");
+  readBtn.className = "btn-read";
+  readBtn.textContent = "Change Read Or Not";
+
   const removeBtn = document.createElement("button");
   removeBtn.className = "btn-remove";
   removeBtn.textContent = "Remove";
@@ -42,6 +51,9 @@ function displayBook() {
   bookDetail.appendChild(title);
   bookDetail.appendChild(author);
   bookDetail.appendChild(pages);
+  bookDetail.appendChild(readShow);
+  bookDetail.appendChild(readBtn);
+  // bookDetail.appendChild(readImage);
   bookDetail.appendChild(removeBtn);
 
   myLibrary.forEach(function (item, index) {
@@ -49,13 +61,34 @@ function displayBook() {
     title.textContent = `${item.title}`;
     author.textContent = `${item.author}`;
     pages.textContent = `${item.pages} + " pages"`;
+    if (item.read == true) {
+      readShow.textContent = "Read";
+    } else if (item.read == false) {
+      readShow.textContent = "Not Read";
+
+      // readImage.style.display = "none";
+    }
+    readBtn.dataset.id = index + 1;
     removeBtn.dataset.id = index + 1;
+  });
+
+  readBtn.addEventListener("click", function changeReadStatus(e) {
+    let indexRead = e.target.dataset.id - 1;
+    let readStatus = myLibrary[indexRead].read;
+    if (readStatus === true) {
+      myLibrary[indexRead].read = false;
+      readShow.textContent = "Not Read";
+      console.log(myLibrary);
+    } else if (readStatus === false) {
+      myLibrary[indexRead].read = true;
+      readShow.textContent = "Read";
+      console.log(myLibrary);
+    }
   });
 
   removeBtn.addEventListener("click", function removeFromLibrary(e) {
     let indexRemove = e.target.dataset.id - 1;
     myLibrary.splice(indexRemove, 1);
-    console.log(bookDetail.parentElement);
     bookDetail.parentElement.remove();
     console.log(myLibrary);
   });
@@ -72,7 +105,14 @@ addBook.addEventListener("click", function (e) {
 });
 
 function addBookToLibrary() {
-  const bookNew = new Book(titleForm.value, authorForm.value, pagesForm.value);
+  const bookNew = new Book(
+    titleForm.value,
+    authorForm.value,
+    pagesForm.value,
+    read.checked
+  );
+
   myLibrary.push(bookNew);
   displayBook();
+  console.log(myLibrary);
 }
